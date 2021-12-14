c73c0fc2fb1f47d4bb81e06b72f87d811b1ed390
Carnell <92267723+c-poteat@users.noreply.github.com>
2021-12-13 19:36:25 -0500

    updated

--- src/main/java/StringFixerTest.java
+++ src/main/java/StringFixerTest.java
@@ -6,12 +6,6 @@ import java.util.regex.Pattern;
 public class StringFixerTest {
     StringFixer stringFixer = new StringFixer();
 
-    @Test
-    public void matcherTest() {
-        String x = "This is a new line=?xxx";
-        Pattern ptrn = Pattern.compile("^(\\\\S+)$", Pattern.MULTILINE);
-        Matcher matcher = ptrn.matcher(x);
-            System.out.println(matcher);
-        }
+
         }
 
